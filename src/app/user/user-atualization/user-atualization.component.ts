d73d4bf199220395dd7195aa0428db4ba43661a9
Israel.Sestrem <israel.sestrem@senior.com.br>
2022-12-12 14:59:44 -0300

    change inputs type to password

--- src/app/user/user-atualization/user-atualization.component.ts
+++ src/app/user/user-atualization/user-atualization.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { HomeService } from 'src/app/home/home.service';
-import { RecUser } from 'src/app/interfaces/user.model';
 import { UserService } from '../user.service';
 
 @Component({
